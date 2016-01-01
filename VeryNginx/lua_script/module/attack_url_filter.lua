c4d755f1fb39a8b258d383ff49ccd5aaee82c457
AlexaZhou <AlexaZhou@163.com>
2016-01-02 00:50:56 +0800

    add attack_url_filter module

---
+++ VeryNginx/lua_script/module/attack_url_filter.lua
@@ -0,0 +1,12 @@
+-- -*- coding: utf-8 -*-
+-- @Date    : 2016-01-02 46:40
+-- @Author  : Alexa (AlexaZhou@163.com)
+-- @Link    : 
+-- @Disc    : filter url request maybe attack
+
+M = {}
+
+function M.filter()
+end
+
+return M
--- VeryNginx/lua_script/on_access.lua
+++ VeryNginx/lua_script/on_access.lua
@@ -1,2 +1,5 @@
+attack_url_filter = require "attack_url_filter"
+attack_url_filter.filter()
+
 router = require "router"
 router.filter()
