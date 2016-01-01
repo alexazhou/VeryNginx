91c44cbcc10914091d0b09f95eae789891714fc2
AlexaZhou <AlexaZhou@163.com>
2016-01-02 00:54:29 +0800

    add VeryNginxConfig module

--- VeryNginx/lua_script/VeryNginxConfig.lua
+++ VeryNginx/lua_script/VeryNginxConfig.lua
@@ -1,12 +1,9 @@
 -- -*- coding: utf-8 -*-
--- @Date    : 2016-01-02 00:46
+-- @Date    : 2016-01-02 00:51
 -- @Author  : Alexa (AlexaZhou@163.com)
 -- @Link    : 
 -- @Disc    : filter url request maybe attack
 
 local M = {}
-
-function M.filter()
-end
 
 return M
