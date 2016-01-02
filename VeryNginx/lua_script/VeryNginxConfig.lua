ddf6e72bf85316d1d32a43faac4ea30df7ba00e7
AlexaZhou <AlexaZhou@163.com>
2016-01-02 12:27:21 +0800

    加入读取配置的接口

--- VeryNginx/lua_script/VeryNginxConfig.lua
+++ VeryNginx/lua_script/VeryNginxConfig.lua
@@ -10,4 +10,16 @@ M.attack_url = {"\\.(git|svn)",
 "\\.(haccess|bash_history|sql)$",
 }
 
+cjson = require "cjson"
+
+function M.report()
+    local report = {}
+    for k, v in pairs(M) do
+        if type( v ) == "table" then
+            report[k] = v
+        end
+    end
+    return cjson.encode(report)
+end
+
 return M
--- VeryNginx/lua_script/module/router.lua
+++ VeryNginx/lua_script/module/router.lua
@@ -5,15 +5,21 @@
 -- -- @Disc    : url router
 
 summary = require "summary"
+VeryNginxConfig = require "VeryNginxConfig"
 
 local M = {}
 
+local url_route = {}
+url_route["/verynginx/summary"] = summary.report
+url_route["/verynginx/config"] = VeryNginxConfig.report
+
 function M.filter() 
     --ngx.log(ngx.STDERR,"run router")
-    if ngx.var.uri == "/nginx/summary" then
+    local handle = url_route[ngx.var.uri]
+    if handle ~= nil then
         ngx.header.content_type = "application/json"
         ngx.header.charset = "utf-8"
-        ngx.say( summary.report()  )
+        ngx.say( handle()  )
         ngx.exit(200)
     end
     --ngx.log(ngx.STDERR,"run router end")
