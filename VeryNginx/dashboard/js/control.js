194a85fa6b455c5f4664a1d6096f8210ead89446
AlexaZhou <AlexaZhou@163.com>
2016-01-04 19:29:07 +0800

    add group display frame

--- VeryNginx/dashboard/js/control.js
+++ VeryNginx/dashboard/js/control.js
@@ -7,13 +7,27 @@ control.init = function(){
 }
 
 control.get_config = function(){
+	$(".init_click").click();
     $.get("/verynginx/config",function(data,status){
         console.log(status);
         console.log(data);
 		control.verynginx_config = data;
 
         var config_json = JSON.stringify(data, null, 2);
-		$("#config_all_show").text(config_json);
+		$("#config_system_all_show").text(config_json);
     }); 
 }
 
+control.switch_to_page = function( page ){
+    $(".page").hide();
+	$("#page_"+page).show();
+
+    $(".topnav").removeClass("active");
+    $("#topnav_"+page).addClass("active");
+}
+
+control.switch_to_configGroup = function( item ){
+	console.log(item);
+	$(".config_group").hide();
+    $("#config_" + $(item).attr("group")).show();
+}
