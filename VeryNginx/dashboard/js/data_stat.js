5ec9e92744ec3f3bbed9ce4c94018fe8cc80396c
lancegin <gin.lance.inside@hotmail.com>
2016-01-19 17:49:32 +0800

    表格呈现各URL／URI详细访问情况

--- VeryNginx/dashboard/js/data_stat.js
+++ VeryNginx/dashboard/js/data_stat.js
@@ -7,43 +7,29 @@ $.ajax({
     success: function (json_data) {
 
         console.log("异步请求成功");
-
+        console.log(typeof json_data);
         var response = json_data;
 
         console.log(response);
 
-        for (var key in json_data) {
-            console.log('key name:' + key + ' value:' + json_data[key].size);
-            var url_details = new Vue({
+        for (var key in response) {
+            console.log(key);
+            
+            // 计算访问成功率
+            var success = json_data[key].status[200] / json_data[key].count;
 
-                el: '#url_details',
+            // 动态增加每一列关于各URL/URI的详细访问信息
+            var dyn_tab =  "<tr><th>##</th>" +
+                           "<th>" + key + "</th>" +
+                           "<th>" + json_data[key].count + "</th>" +
+                           "<th>" + json_data[key].size + "</th>" +
+                           "<th>" + json_data[key].avg_size + "</th>" +
+                           "<th>" + success.toFixed(4) * 100 + "%</th>" +
+                           "<th>" + json_data[key].time + "</th>" +
+                           "<th>" + json_data[key].avg_time + "</th></tr>";
 
-                data: {
-                    url_details: [
-                        { key: key },
-                        { count: json_data[key].count },
-                        { size: json_data[key].size },
-                        { avg_size: json_data[key].avg_size },
-                        { status: json_data[key].status },
-                        { time: json_data[key].time },
-                        { avg_time: json_data[key].avg_time }
-                    ]
-                }
-
-            })
+            $('#url_details').append(dyn_tab);
+   
         }
     }
 })
-
-
-// var data_stat = '访问统计';
-
-// var data_stat = new Vue({
-
-//     el: '#data_stat',
-
-//     data: {
-//         message: data_stat
-//     }
-// })
-
