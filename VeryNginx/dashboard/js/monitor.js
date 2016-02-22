ca5a9e38cc018230beffe1a7f3d99702eb8991c5
AlexaZhou <AlexaZhou@163.com>
2016-02-22 11:43:13 +0800

    Fix calculate error on avg response time in status page

--- VeryNginx/dashboard/js/monitor.js
+++ VeryNginx/dashboard/js/monitor.js
@@ -252,7 +252,10 @@ monitor.refresh = function(){
             var time_str = monitor.time_str();
             var sub_label = '';
             var response_time_change = data['response_time_total'] - monitor.latest_status['response_time_total'];
-            var avg_response_time = 1000 * response_time_change / time_change;
+            var avg_response_time = 0;
+            if( requests_all_change != 0 ){
+                avg_response_time = 1000 * response_time_change / requests_all_change ;
+            }
             
             var traffic_read_change = data['traffic_read'] - monitor.latest_status['traffic_read'];
             var traffic_write_change = data['traffic_write'] - monitor.latest_status['traffic_write'];
