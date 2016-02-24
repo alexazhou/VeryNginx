f0098ff4beaacafe59e1cc97118c836e89fc820c
AlexaZhou <AlexaZhou@163.com>
2016-02-24 21:42:52 +0800

    Add module "browser_verify"

---
+++ VeryNginx/lua_script/module/browser_verify.lua
@@ -0,0 +1,18 @@
+-- -*- coding: utf-8 -*-
+-- @Date    : 2016-02-24 
+-- @Author  : Alexa (AlexaZhou@163.com)
+-- @Link    : 
+-- @Disc    : verify that the remote client is a browser
+
+local _M = {}
+
+local VeryNginxConfig = require "VeryNginxConfig"
+local request_tester = require "request_tester"
+
+
+function _M.run()
+    
+
+end
+
+return _M
