679ffbcae99cef6416bd394f535bcd2168fe9753
AlexaZhou <AlexaZhou@163.com>
2016-02-29 00:36:33 +0800

    now can modify redirect rules

--- VeryNginx/dashboard/js/form.js
+++ VeryNginx/dashboard/js/form.js
@@ -1,33 +1,38 @@
 var form = new Object();
 
 
-form.get_data = function( form ){
+form.get_data = function( form_id ){
     var data = {}
-    var inputs = $(form).find("input,checkbox,select");
+    var inputs = $('#' + form_id).find("input,checkbox,select");
 
     for( var i=0; i < inputs.length; i++ ){
         var item = $(inputs[i]);
-        if( item.prop('tagName').toLowerCase() == "input" && item.attr('type') == "checkbox" ){
+        var name = item.attr('name');
+        if( item.prop('tagName').toLowerCase() == "input"  ){
 
-            var config_group = item.attr('config_group');
-            if( config_group != null ){
-                if( data[config_group] == null ){
-                    data[config_group] = [];
-                }
+            if( item.attr('type') == "checkbox" ){
+                var config_group = item.attr('config_group');
+                if( config_group != null ){
+                    if( data[config_group] == null ){
+                        data[config_group] = [];
+                    }
 
-                if( item.prop( "checked" ) ){
-                    data[config_group].push( item.attr('name') );
+                    if( item.prop( "checked" ) ){
+                        data[config_group].push( name );
+                    }
+                }else{
+                    if( item.prop( "checked" )){
+                        data[name] = true;
+                    }else{
+                        data[name] = false;
+                    }
                 }
             }else{
-                if( item.prop( "checked" )){
-                    data[item.attr('name')] = true;
-                }else{
-                    data[item.attr('name')] = false;
-                }
+                data[name] = item.val();
             }
 
         }else if( item.prop('tagName').toLowerCase() == "select" ){
-            data[ item.attr('name') ] = item.val();
+            data[ name ] = item.val();
         }
     }
 
@@ -43,23 +48,26 @@ form.set_data = function( form_id, data ){
         var item = $(inputs[i]);
         var name = item.attr('name');
         console.log('process item',item)
-        if( item.prop('tagName').toLowerCase() == "input" && item.attr('type') == "checkbox" ){
-            console.log('flag 1')
-            var config_group = item.attr('config_group');
-            if( config_group != null && data[config_group] != null ){
+        if( item.prop('tagName').toLowerCase() == "input" ){
+            if( item.attr('type') == "checkbox" ){
+                var config_group = item.attr('config_group');
+                if( config_group != null && data[config_group] != null ){
 
-                if( name in data[config_group] ){
-                    item.prop('checked',true);
+                    if( name in data[config_group] ){
+                        item.prop('checked',true);
+                    }else{
+                        item.prop('checked',false);
+                    }
+
                 }else{
-                    item.prop('checked',false);
+                    if( data[name] == true ){
+                        item.prop('checked',true);
+                    }else{
+                        item.prop('checked',false);
+                    }
                 }
-
             }else{
-                if( data[name] == true ){
-                    item.prop('checked',true);
-                }else{
-                    item.prop('checked',false);
-                }
+                item.val( data[name] );
             }
 
         }else if( item.prop('tagName').toLowerCase() == "select" ){
