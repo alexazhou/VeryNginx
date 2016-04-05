b7332dd6f6fd2d5c09f6e20230534dc04541890f
AlexaZhou <AlexaZhou@163.com>
2016-04-06 00:04:17 +0800

    Add a install script

---
+++ install.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-04-04 23:48
+# @Author  : Alexa (AlexaZhou@163.com)
+# @Link    : 
+# @Disc    : 
+
+def main():
+    pass
+
+    #step 1, download openresty
+    
+    #step 2, configure && compile && install openresty
+    
+    #step 3, install VeryNginx file
+
+
+if __name__ == '__main__':
+    main()
+
+    input('Press any key to exit.')
+else:
+    print ('install.py had been imported as a module')
+
