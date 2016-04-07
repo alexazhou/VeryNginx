6e1a2be9c45a67eb08bc5d5773498327561d441d
AlexaZhou <AlexaZhou@163.com>
2016-04-08 00:12:16 +0800

    install.py can install openresty now

--- install.py
+++ install.py
@@ -2,23 +2,47 @@
 # -*- coding: utf-8 -*-
 # @Date    : 2016-04-04 23:48
 # @Author  : Alexa (AlexaZhou@163.com)
-# @Link    : 
-# @Disc    : 
+# @Link    : https://github.com/alexazhou/VeryNginx
+# @Disc    : install VeryNginx
+
+import os
+
+openresty_pkg_url = 'https://openresty.org/download/openresty-1.9.7.3.tar.gz'
+openresty_pkg = 'openresty-1.9.7.3.tar.gz'
 
 def main():
-    pass
 
+    work_path = os.curdir
+
+    #check if the old version of VeryNginx installed
+    if os.path.exists('/opt/VeryNginx/VeryNginx') == True:
+        print("Seems that a old version of VeryNginx was installed in /opt/VeryNginx/...\nBefore install, please delete it and backup the configs if you need.")
+        return
+    
+    #makesure the dir is clean
+    print('### makesure the work directory is clean')
+    #os.system('rm ' + openresty_pkg)
+    #os.system('rm -rf ' + openresty_pkg.replace('.tar.gz',''))
+    
     #step 1, download openresty
+    #print('### start download openresty package...')
+    #os.system( 'wget ' + openresty_pkg_url )
     
+    print('### release the package ...')
+    #os.system( 'tar -xzf ' + openresty_pkg )
+
     #step 2, configure && compile && install openresty
-    
+    os.chdir( openresty_pkg.replace('.tar.gz','') )
+    os.system( './configure --prefix=/opt/verynginx/openrestry --user=nginx --group=nginx --with-http_stub_status_module --with-luajit' )
+    os.system( 'make' )
+    os.system( 'make install' )
+
     #step 3, install VeryNginx file
 
 
 if __name__ == '__main__':
     main()
 
-    input('Press any key to exit.')
 else:
     print ('install.py had been imported as a module')
 
