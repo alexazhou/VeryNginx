ec4cee0d496b7cc7d2fe109b6820cd3644b40aab
AlexaZhou <AlexaZhou@163.com>
2016-04-09 17:06:24 +0800

    improve install script

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ import getopt
 openresty_pkg_url = 'https://openresty.org/download/openresty-1.9.7.3.tar.gz'
 openresty_pkg = 'openresty-1.9.7.3.tar.gz'
 
-work_path = os.curdir
+work_path = os.getcwd()
 
 def install_openresty( ):
     #check if the old version of VeryNginx installed
@@ -22,40 +22,62 @@ def install_openresty( ):
     
     #makesure the dir is clean
     print('### makesure the work directory is clean')
-    exec('rm ' + openresty_pkg)
-    exec('rm -rf ' + openresty_pkg.replace('.tar.gz',''))
+    exec_sys_cmd('rm -rf ' + openresty_pkg.replace('.tar.gz',''))
     
     #download openresty
-    print('### start download openresty package...')
-    exec( 'wget ' + openresty_pkg_url )
+    down_flag = True
+    if os.path.exists( './' + openresty_pkg ):
+        ans = ''
+        while ans not in ['y','n']:
+            ans = common_input(' Found %s in current directory, use it?(y/n)'%openresty_pkg)
+        if ans == 'y':
+            down_flag = False
+
+    if down_flag == True:
+        print('### start download openresty package...')
+        exec_sys_cmd('rm -f ' + openresty_pkg)
+        exec_sys_cmd( 'wget ' + openresty_pkg_url )
+    else:
+        print('### use local openresty package...')
     
     print('### release the package ...')
-    exec( 'tar -xzf ' + openresty_pkg )
+    exec_sys_cmd( 'tar -xzf ' + openresty_pkg )
 
     #configure && compile && install openresty
     print('### configure openrestry ...')
     os.chdir( openresty_pkg.replace('.tar.gz','') )
-    exec( './configure --prefix=/opt/verynginx/openrestry --user=nginx --group=nginx --with-http_stub_status_module --with-luajit' )
+    #exec_sys_cmd( './configure --prefix=/opt/verynginx/openrestry --user=nginx --group=nginx --with-http_stub_status_module --with-luajit' )
     
     print('### compile openrestry ...')
-    exec( 'make' )
+    #exec_sys_cmd( 'make' )
     
     print('### install openrestry ...')
-    exec( 'make install' )
+    #exec_sys_cmd( 'make install' )
 
 def install_verynginx():
     #install VeryNginx file
     print('### copy VeryNginx files ...')
     os.chdir( work_path )
-    exec( 'cp -r -f ./verynginx /opt/verynginx/verynginx' )
+    exec_sys_cmd( 'pwd' )
+    if os.path.exists('/opt/verynginx/') == False:
+        exec_sys_cmd( 'mkdir -p /opt/verynginx' )
+    
+    exec_sys_cmd( 'cp -r -f ./verynginx /opt/verynginx/verynginx' )
 
 def update_verynginx():
     install_verynginx()    
 
-def exec( cmd ):
+
+def exec_sys_cmd(cmd):
     print( cmd )
     os.system( cmd )
 
+
+def common_input( s ):
+    if sys.version_info.major == 3:
+        return input( s )
+    else:
+        return raw_input( s )
 
 def safe_pop(l):
     if len(l) == 0:
@@ -100,6 +122,8 @@ if __name__ == '__main__':
             show_help_and_exit()
     else:
         show_help_and_exit()
+
+    print('All work finished successfully, enjoy it~')
 
 
 else:
