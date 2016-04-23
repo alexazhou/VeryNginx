940125170e59d5f83e4e586f1b87144b8282e4bb
AlexaZhou <AlexaZhou@163.com>
2016-04-24 00:58:40 +0800

    1，Set header when proxy pass to the backend 2，hide log

--- verynginx/lua_script/on_banlance.lua
+++ verynginx/lua_script/on_banlance.lua
@@ -19,10 +19,10 @@ function _M.find_node( node_list )
     for name,node in pairs( node_list ) do
         rate_sum = rate_sum + tonumber(node['rate']) 
     end
-    ngx.log( ngx.ERR, 'rate_sum',rate_sum)
+    --ngx.log( ngx.ERR, 'rate_sum',rate_sum)
     
     local p = math.random( rate_sum )
-    ngx.log( ngx.ERR, 'p',p)
+    --ngx.log( ngx.ERR, 'p',p)
 
     for name,node in pairs( node_list ) do
         local tmp = tonumber(node['rate'])
@@ -36,17 +36,17 @@ end
 
 
 function _M.run()
-    ngx.log( ngx.ERR, "---ngx.balancer---")
-    ngx.log( ngx.ERR, ngx.var.vn_proxy_target)
+    --ngx.log( ngx.ERR, "---ngx.balancer---")
+    --ngx.log( ngx.ERR, ngx.var.vn_proxy_target)
     
     local rule = VeryNginxConfig.configs['backend_upstream'][ngx.var.vn_proxy_target]
     local node = _M.find_node( rule['node'] )
     
-    ngx.log( ngx.ERR, 'ip',node['ip'],node['port'])
+    --ngx.log( ngx.ERR, 'ip',node['ip'],node['port'])
 
     local ok, err = balancer.set_current_peer( node['ip'], node['port'])
     if not ok then
-        ngx.log(ngx.ERR, "failed to set the current peer: ", err)
+        --ngx.log(ngx.ERR, "failed to set the current peer: ", err)
         return ngx.exit(500)
     end
 
