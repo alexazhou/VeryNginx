097449faac7afe8b8cdb765454dc51dd2c74a3b9
AlexaZhou <AlexaZhou@163.com>
2016-05-25 00:39:56 +0800

    Fix verify logic

--- verynginx/dashboard/js/verify.js
+++ verynginx/dashboard/js/verify.js
@@ -133,55 +133,6 @@ verify.AddrOf101Message =  function(min,max)
     return handle;
 }
 
-//有且仅仅有四个字节的字符串,仅限'0'-'9'
-verify.AddrOf101 =  function(min,max)
-{
-
-    var handle = function(v){
-        
-        if(v.length == 0)//地址可以不填写
-            return '';
-
-        if(v.length != 4 )
-            return "需要是有且仅仅有四个字符的字符串,仅限'0'-'9'";
-
-        var map = '0123456789';//允许的字符集
-        for(var i=0;i<v.length;i++){
-            
-            var char = v[i];
-            if(map.indexOf(char) <0)
-                return "需要是有且仅仅有四个字符的字符串,仅限'0'-'9'";
-    
-        }
-
-        return "";
-    }
-
-    return handle;
-}
-
-//校验重连最大间隔
-//可以为15-600.按数字下发
-//或者为”troy“.按字符串下发
-verify.dailInterval = function()
-{
-    var handle = function(v)
-    {
-        var err_message = "必须为15-600之间的整数";
-
-        var core1 = verify.range( 15,600 );
-        
-        if( core1(v) == "")//在数字范围内
-            return "";
-
-        if( v == "troy" )
-            return "";
-
-        return err_message;
-    }
-
-    return handle;
-}
 
 //校验是否合法的ip地址
 verify.ip =function ()
@@ -222,7 +173,7 @@ verify.uri = function(){
             return "URI require to start with '/' ";
         }
 
-        if( v.lastIndexOf('/')  == v.length - 1  ){
+        if( v.length > 1 && v.lastIndexOf('/')  == v.length - 1  ){
             return "URI should not end with '/' ";
         }
 
